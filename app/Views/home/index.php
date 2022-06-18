2ee2dbf0350efa4b1f4f56851274b3a2a3dab1a6
Patryk Pawlowski <ppawlowski96@gmail.com>
2022-06-18 14:19:49 +0200

    experience

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,8 +1,8 @@
 
 <section id="top" class="w-100 position-relative" style="height: 75vh">
     <div id="background" class="w-100 position-absolute overflow-hidden" style="height: 75vh"></div>
-    <div class="container-fluid h-100">
-        <div class="row h-100 w-50">
+    <div class="container-fluid h-100 d-flex justify-content-center align-items-center">
+        <div class="row h-25 w-25">
             <div class="col-md-12 h-100 d-flex flex-column justify-content-center align-items-center text-white text-center py-5" style="z-index: 2">
             <img src="<?= base_url('assets/images/logo/logo-page-white.png') ?>" />
                 <h1 class="mt-5 fs-4r">Patryk Pawłowski</h1>
@@ -41,26 +41,99 @@
 </section>
 <section id="experience" class="py-5">
     <div class="container">
-    <div class="row">
+    <div class="row my-2">
             <div class="col-md-12 text-center">
                 <h1>Technology I use</h1>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-3 d-flex justify-content-center align-items-center">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="round-dot" >
-                            <span class="card-icon">
-                                4
-                            </span>
+            <div class="col-md-12">
+                <div id="carouselExampleCaptions" class="carousel carousel-dark slide" data-bs-ride="false">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="PHP 7.4/8.1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Codeigniter 4"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Visual Studio Code"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Mysql"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Javascript"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="HTML"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6" aria-label="Bootstrap 5"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="w-100 d-flex justify-content-center align-items-center" style="margin-bottom: 150px">
+                                <img width="200" src="<?= base_url('assets/images/php-icon.png') ?>" />
+                            </div>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5 class="fs-3">PHP 7.4/8.1</h5>
+                                <p class="lead">For making applications back-end work (auth, data operations)</p>
+                            </div>
                         </div>
+                        <div class="carousel-item">
+                            <div class="w-100 d-flex justify-content-center align-items-center" style="margin-bottom: 150px">
+                                <img width="200" src="<?= base_url('assets/images/ci-icon.png') ?>" />
+                            </div>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5 class="fs-3">Codeigniter 4</h5>
+                                <p class="lead">Framework for PHP to make coding easier for me</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="w-100 d-flex justify-content-center align-items-center" style="margin-bottom: 150px">
+                                <img width="200" src="<?= base_url('assets/images/vsc-icon.png') ?>" />
+                            </div>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5 class="fs-3">Visual Studio Code</h5>
+                                <p class="lead">Here I'm writing all of this stuff</p>
+                            </div>
+                        </div>          
+                        <div class="carousel-item">
+                            <div class="w-100 d-flex justify-content-center align-items-center" style="margin-bottom: 150px">
+                                <img width="200" src="<?= base_url('assets/images/mysql-icon.png') ?>" />
+                            </div>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5 class="fs-3">Mysql</h5>
+                                <p class="lead">Databases, queries, data operations</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="w-100 d-flex justify-content-center align-items-center" style="margin-bottom: 150px">
+                                <img width="200" src="<?= base_url('assets/images/js-icon.png') ?>" />
+                            </div>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5 class="fs-3">Javascript</h5>
+                                <p class="lead">Some magic lines of code, to give application live</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="w-100 d-flex justify-content-center align-items-center" style="margin-bottom: 150px">
+                                <img width="200" src="<?= base_url('assets/images/html-icon.png') ?>" />
+                            </div>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5 class="fs-3">HTML</h5>
+                                <p class="lead">Just like doing the puzzles :)</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="w-100 d-flex justify-content-center align-items-center" style="margin-bottom: 150px">
+                                <img width="200" src="<?= base_url('assets/images/bootstrap-icon.png') ?>" />
+                            </div>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5 class="fs-3">Bootstrap 5</h5>
+                                <p class="lead">To make it all look nice</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                     </div>
                 </div>
             </div>
-            <div class="col-md-3 d-flex justify-content-center align-items-center"></div>
-            <div class="col-md-3 d-flex justify-content-center align-items-center"></div>
-        </div>
     </div>
 </section>
 <!-- <section class="main position-absolute top-50 start-50 translate-middle py-5">
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,10 @@
 particlesJS.load('background', './assets/js/particle-js/particles.json', function() {
     console.log('particles.js loaded - callback');
 });
+
+const myCarouselElement = document.querySelector('#carouselExampleCaptions')
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 3000,
+  wrap: true,
+  ride: true,
+})
